481e4f541c970c68861de902df2fd3e364899865
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-07-02 12:51:21 +0400

    feat: table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,40 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: -999, b: 999, action: Action.Add, expected: 0 },
+  { a: 3, b: -2, action: Action.Add, expected: 1 },
+  {
+    a: Number.MIN_SAFE_INTEGER,
+    b: Number.MAX_SAFE_INTEGER,
+    action: Action.Add,
+    expected: 0,
+  },
+
+  { a: 555, b: 110, action: Action.Subtract, expected: 445 },
+
+  { a: 1, b: 5, action: Action.Multiply, expected: 5 },
+  { a: 0, b: 5, action: Action.Multiply, expected: 0 },
+  { a: 10, b: 10, action: Action.Multiply, expected: 100 },
+
+  { a: 5, b: 2, action: Action.Divide, expected: 2.5 },
+  { a: 5, b: 0, action: Action.Divide, expected: Infinity },
+  { a: 0, b: 5, action: Action.Divide, expected: 0 },
+
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 2, b: 0, action: Action.Exponentiate, expected: 1 },
+
+  { a: 1, b: 1, action: 'invalid action', expected: null },
+
+  { a: 123n, b: 999, action: Action.Add, expected: null },
+  { a: 5, b: '123', action: Action.Divide, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'should return expected result for valid input and null for invalid input',
+    ({ a, b, action, expected }) => {
+      const result = simpleCalculator({ a, b, action });
+      expect(result).toBe(expected);
+    },
+  );
 });
